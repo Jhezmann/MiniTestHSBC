d0f2b007def4ce09167622908da6d564e4ec4f0c
Jhezmann <782265846@qq.com>
2021-03-25 18:02:37 +0800

    Completed

--- Mini_Test_HSBC/src/main/java/cn/jhezmann/config/MainJavaConfig.java
+++ Mini_Test_HSBC/src/main/java/cn/jhezmann/config/MainJavaConfig.java
@@ -2,8 +2,6 @@ package cn.jhezmann.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("cn.jhezmann")
