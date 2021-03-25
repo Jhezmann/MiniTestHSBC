a01040709a7e42fdeab1bab7fee77c855dfdf9b5
Jhezmann <782265846@qq.com>
2021-03-25 18:01:20 +0800

    Completed

--- Mini_Test_HSBC/src/main/java/cn/jhezmann/core/MiniTest.java
+++ Mini_Test_HSBC/src/main/java/cn/jhezmann/core/MiniTest.java
@@ -4,24 +4,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MiniTest {
-    private final String[] LETTERS = {"","","abc","def","ghi","jkl","mmo","pqrs","tuv","wxyz"};
+    //using index to cover income digits
+    private String[] LETTERS = {"","","abc","def","ghi","jkl","mmo","pqrs","tuv","wxyz"};
 
-    private int length = -1;
-
+    private int length = 0;
     private String[] strArray;
 
     public String test(int[] digits){
 
-        /*for (int i = 0; i < digits.length; i++) {
-            if(digits[i]==0||digits[i]==1){
-
-            }
-        }*/
-
         length = digits.length;
-
         strArray = new String[length];
 
+        //Build up strArray
         for (int i = 0; i < length; i++) {
             String letters = LETTERS[digits[i]];
             strArray[i] = letters;
@@ -29,36 +23,44 @@ public class MiniTest {
 
         return conbinator("",0);
 
-
     }
-    
+
+    public void changeLETTERS(){
+        for (int i = 0; i < LETTERS.length; i++) {
+            LETTERS[i] = i + LETTERS[i];
+        }
+    }
+
     private String conbinator(String s, int index) {
         int count = 0;
         String[] arrStr;
-        for (int i = index; i < length; i++) {
-            String str = strArray[i];
-            String[] arr1= s.split(",");
-            String[] arr2= str.split("");
-            arrStr = new String[arr1.length*arr2.length];
-            for (int j = 0; j < arr1.length; j++) {
-                for (int k = 0; k < arr2.length; k++) {
-                    arrStr[count] = arr1[j]+arr2[k];
-                    count++;
-                }
-            }
-            s = "";
-            for (int j = 0; j < arrStr.length; j++) {
-                if(!s.equals("")){
-                    s += ",";
-                }
-                s += arrStr[j];
-            }
-            if(i+1==length){
-                return s;
-            }else{
-                return conbinator(s,i+1);
+        String str = strArray[index];
+
+        //Split original arr and combiner;
+        String[] arr1= s.split(" ");
+        String[] arr2= str.split("");
+
+        arrStr = new String[arr1.length*arr2.length];
+
+        for (int j = 0; j < arr1.length; j++) {
+            for (int k = 0; k < arr2.length; k++) {
+                arrStr[count] = arr1[j]+arr2[k];
+                count++;
             }
         }
-        return "";
+
+        s = "";
+
+        for (int j = 0; j < arrStr.length; j++) {
+            if(!s.equals("")){
+                s += " ";
+            }
+            s += arrStr[j];
+        }
+        if(index+1==length){
+            return s;
+        }else{
+            return conbinator(s,index+1);
+        }
     }
 }
--- Mini_Test_HSBC/src/test/java/cn/jhezmann/RunTest.java
+++ Mini_Test_HSBC/src/test/java/cn/jhezmann/RunTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RunTest {
@@ -17,31 +18,59 @@ public class RunTest {
         ioc = new AnnotationConfigApplicationContext(MainJavaConfig.class);
     }
 
+    /**
+     * Test 01
+     * Get input digits and output letter combination
+     */
     @Test
-    public void printLetterCombination(){
+    public void letterCombination(){
 
+        MiniTest mt = new MiniTest();
+        System.out.println(mt.test(getDigitArray()));
+
+    }
+
+    /**
+     * Test 02
+     * Get input digits and output letter combination converting number;
+     */
+    @Test
+    public void letterConvertNumber(){
+
+        MiniTest mt = new MiniTest();
+        mt.changeLETTERS();
+        System.out.println(mt.test(getDigitArray()));
+    }
+
+    /**
+     * Use scaner to get input digits
+     * @return return digit array
+     */
+    private int[] getDigitArray(){
         Scanner input=new Scanner(System.in);
         String str = input.next();
         String[] strArr = str.split("");
-        int index = 0;
-        int[] nl = new int[index];
+        int[] digitArr = new int[0];
         for (int i = 0; i < strArr.length; i++) {
             try{
                 int x = Integer.parseInt(strArr[i]);
-                nl[index++] = x;
+                digitArr= Arrays.copyOf(digitArr, digitArr.length+1);
+                digitArr[digitArr.length-1] = x;
             }catch (Exception e){
-
+                System.out.println(e.getMessage());
             }
         }
-        MiniTest mt = new MiniTest();
-        System.out.println(mt.test(nl));
-
+        return digitArr;
     }
 
+    /**
+     * main function for IDEA user who cannot input in the console during the test.
+     * @param args
+     */
     public static void main(String[] args){
         RunTest rt = new RunTest();
-        rt.printLetterCombination();
+        rt.letterCombination();
+        rt.letterConvertNumber();
     }
-
 
 }
